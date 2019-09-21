bafbd936a011e569045a93dce9cfeb1dedfc1fec
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-21 16:04:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 reverse_each_word = "Hello there, and how are you?")
 
 def reverse_each_word
-  reverse=[]
-  string.split().each { |letter|   << letter}
+  letters_array = string.split()
+  .each { |letter| array << letter}
+  
 end
 
