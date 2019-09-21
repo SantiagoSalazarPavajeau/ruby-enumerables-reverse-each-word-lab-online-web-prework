c378ebc22e71851442bdd911dd7f9683d095d986
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-09-21 16:56:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word
   array = string.split(" ")
-  reverse_array = array.collect { |letter| reverse_array.unshift(letter)}
-  reverse_string = reverse_array.join()
+  reverse_string = array.collect { |letter| letter.reverse}.join(",")
+  return reverse_string
 end
 
